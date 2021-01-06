6f11e0503b13b369b92f0e0945b2e2f281fa2137
Artyom Pugachev <artempugachev1@gmail.com>
2021-01-06 22:13:42 +0300

    center button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      
+      <div className="controls">
+        <button className="play-white">White Noise</button>
+      </div>
     </div>
   );
 }
