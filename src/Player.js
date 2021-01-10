d687710ffafe3939fb7cce783dab19064fdc584c
Artyom Pugachev <artempugachev1@gmail.com>
2021-01-10 16:12:03 +0300

    play sound on button

--- src/Player.js
+++ src/Player.js
@@ -1,39 +1,46 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Player.css';
 
 
 function Player() {
+    const [play, handlePlay] = useState(false);
+
+    const togglePlay = () => handlePlay(!play);
+
+    const toggleWhiteClick = togglePlay;
+
+
     useEffect(() => {
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-        const noiseDuration = 2;
-
-        const audioContext = new AudioContext();
-        const bufferSize = audioContext.sampleRate * noiseDuration;
-        const buffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
-        let data = buffer.getChannelData(0);
-
-        for (let i = 0; i < bufferSize; i++) {
-            data[i] = Math.random() * 2 - 1;
+        if (play) {
+            const AudioContext = window.AudioContext || window.webkitAudioContext;
+            
+            const noiseDuration = 2;
+    
+            const audioContext = new AudioContext();
+            const bufferSize = audioContext.sampleRate * noiseDuration;
+            const buffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+            let data = buffer.getChannelData(0);
+    
+            for (let i = 0; i < bufferSize; i++) {
+                data[i] = Math.random() * 2 - 1;
+            }
+    
+            let noise = audioContext.createBufferSource();
+            noise.buffer = buffer; 
+            console.log('sound');
+            noise.connect(audioContext.destination);
+            noise.start();
         }
-
-        let noise = audioContext.createBufferSource();
-        noise.buffer = buffer; 
-
-        noise.connect(audioContext.destination);
-        noise.start();
-
-        // todo Player.js:24 The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page. https://goo.gl/7K7WLu
 
         return () => {
             
         }
-    }, [])
+    }, [play]);
 
 
     return (
         <div className="player">
-            <button className="play-white">White Noise</button>
+            <button className="play-white" onClick={toggleWhiteClick}>White Noise</button>
         </div>
     )
 }
