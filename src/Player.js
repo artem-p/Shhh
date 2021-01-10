a60d6952dfc44ed4570d07724c1d1eb0597f431f
Artyom Pugachev <artempugachev1@gmail.com>
2021-01-10 22:46:59 +0300

    start stop on button, not working

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,7 @@ import './Player.css';
 
 
 function Player() {
+    const [connect, handleConnect] = useState(false);
     const [play, handlePlay] = useState(false);
 
     const togglePlay = () => handlePlay(!play);
@@ -10,34 +11,41 @@ function Player() {
     const toggleWhiteClick = togglePlay;
 
 
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    const noiseDuration = 2;
+    
+    const audioContext = new AudioContext();
+    const bufferSize = audioContext.sampleRate * noiseDuration;
+    const buffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
+    let data = buffer.getChannelData(0);
+
+    for (let i = 0; i < bufferSize; i++) {
+        data[i] = Math.random() * 2 - 1;
+    }
+
+    let noise = audioContext.createBufferSource();
+    noise.buffer = buffer; 
+    noise.loop = true;
+    noise.start();
+    
     useEffect(() => {
         if (play) {
-            const AudioContext = window.AudioContext || window.webkitAudioContext;
-            
-            const noiseDuration = 2;
-    
-            const audioContext = new AudioContext();
-            const bufferSize = audioContext.sampleRate * noiseDuration;
-            const buffer = audioContext.createBuffer(1, bufferSize, audioContext.sampleRate);
-            let data = buffer.getChannelData(0);
-    
-            for (let i = 0; i < bufferSize; i++) {
-                data[i] = Math.random() * 2 - 1;
-            }
-    
-            let noise = audioContext.createBufferSource();
-            noise.buffer = buffer; 
             noise.connect(audioContext.destination);
-            noise.loop = true;
-            noise.start();
+            handleConnect(true);
         } else {
+            if (connect) {
+                noise.disconnect(audioContext.destination);
+                handleConnect(false);
+            }
 
+            // noise.stop();
         }
 
         return () => {
             
         }
-    }, [play]);
+    }, [play, noise]);
 
 
     return (
